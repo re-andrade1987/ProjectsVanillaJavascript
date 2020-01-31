320edf2fb646e63653fd4f0e0becb2000deffc5e
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-31 12:36:30 -0300

    Finalizando projeto

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -1,4 +1,4 @@
-const main = () =>{
+const main = () => {
     const song = document.querySelector('.song')
     const play = document.querySelector('.play')
     const outline = document.querySelector('.movie-outline circle')
@@ -8,6 +8,8 @@ const main = () =>{
     const sounds = document.querySelectorAll('.sound-picker button')
     // Time display
     const timeDisplay = document.querySelector('.time-display')
+    const timeSelect = document.querySelectorAll('.time-select button')
+
 
     const outLineLength = outline.getTotalLength()
     console.log(outLineLength)
@@ -18,17 +20,64 @@ const main = () =>{
     outline.style.strokeDasharray = outLineLength
     outline.style.strokeDashoffset = outLineLength
 
-    //play sound 
-    play.addEventListener("click", () =>{
-        song.play()
+    //Pick different sounds
+    sounds.forEach(sound =>{
+        sound.addEventListener('click', function(){
+        song.src = this.getAttribute('data-sound')
+        video.src = this.getAttribute('data-video')
+        checkPlaying(song)
+
+        })
     })
 
 
+    //play sound 
+    play.addEventListener("click", () => {
+        checkPlaying(song)
+    })
 
+    //Select sound 
+    timeSelect.forEach(option => {
+        option.addEventListener("click", function() {
+            fakeDuration = this.getAttribute('data-time')
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:
+            ${Math.floor(fakeDuration % 60)}`
+        })
+    })
 
+//function specific sound to stop and play de sounds
 
+const checkPlaying = song => {
+    if (song.pause) {
+        song.play()
+        video.play()
+        play.src = './svg/pause.svg'
+    } else {
+        song.pause()
+        video.pause()
+        play.src = './svg/play.svg'
+    }
+}
 
+// We can Animated the circle 
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime
+    let elapsed = fakeDuration - currentTime
+    let seconds = Math.floor(elapsed % 60)
+    let minutes = Math.floor(elapsed / 60)
 
+    // Animated the circle 
+    let progress = outLineLength - (currentTime / fakeDuration) * outLineLength
+    outline.style.strokeDashoffset = progress
+    timeDisplay.textContent = `${minutes}:${seconds}`
+
+    if(currentTime >= fakeDuration){
+        song.pause()
+        song.currentTime = 0
+        play.src = './svg/play.svg'
+        video.pause()
+    }
+    }
 
 }
 

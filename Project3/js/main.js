59b67e99f913c64a0be732d860b6c3b4ab6dd622
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-31 16:27:21 -0300

    Trazendo dados da api

---
+++ Project3/js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', () => {
+    let long
+    let lat
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position)
+            long = position.coords.longitude
+            lat = position.coords.latitude
+
+            const proxy = "https://cors-anywhere.herokuapp.com/"
+            const api = `${proxy}https://api.darksky.net/forecast/28b97ad61a4b0d8fbaefe4aa585418bb/${lat},${long}`
+            fetch(api)
+                .then(response => {
+                    return response.json()
+
+                })
+                .then(data => {
+                    console.log(data)
+                })
+
+        })
+    }
+})

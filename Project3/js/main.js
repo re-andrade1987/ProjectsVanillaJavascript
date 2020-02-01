2ca38ef20c76ec3f51b675f1fae36f993cde9bdc
re-andrade1987 <renataandrade1987@hotmail.com>
2020-01-31 22:13:56 -0300

    Ajustando layout

--- Project3/js/main.js
+++ Project3/js/main.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', () => {
     let long
     let lat
+    let tempatureDescription = document.querySelector('.temperature-description')
+    let tempatureDegree = document.querySelector('.temperature-degree')
+    let locationTimeZone = document.querySelector('.location-timezone')
+
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             console.log(position)
@@ -16,7 +21,14 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data)
+                    const { temperature, summary } = data.currently
+                    tempatureDescription.textContent = temperature
+                    tempatureDegree.textContent = summary
+                    locationTimeZone.textContent = data.timezone
+
+
                 })
+
 
         })
     }
